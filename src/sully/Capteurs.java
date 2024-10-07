01aef3e124d2ae7ecd69072bfb29474d54f9b7e1
evaski <evaski@live.fr>
2024-10-07 16:21:57 +0200

    la classe est vide

--- src/sully/Capteurs.java
+++ src/sully/Capteurs.java
@@ -1,25 +1,6 @@
 package sully;
 
 public class Capteurs {
-<<<<<<< HEAD
-<<<<<<< HEAD
-	public static void main(String [] args) {
-		System.out.println("hello sully"); 
-	}
-=======
-	
-	public static void main(String [] args) {
-		
-		system.out.println("hello");
-		
-	}
-	
-	
-=======
-
->>>>>>> branch 'master' of https://github.com/evaski/projet_sully_ia.git
-
->>>>>>> branch 'master' of https://github.com/evaski/projet_sully_ia.git
 }
 
 
