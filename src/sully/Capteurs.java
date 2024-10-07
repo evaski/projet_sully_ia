a7b609e0e0c5a75d6e0701aa6b596dc326eadebf
Anesie <anesie.martiniani@outlook.fr>
2024-10-07 16:20:58 +0200

    encore anesie

--- src/sully/Capteurs.java
+++ src/sully/Capteurs.java
@@ -1,25 +1,15 @@
 package sully;
 
 public class Capteurs {
-<<<<<<< HEAD
-<<<<<<< HEAD
-	public static void main(String [] args) {
-		System.out.println("hello sully"); 
-	}
-=======
+
 	
 	public static void main(String [] args) {
 		
-		system.out.println("hello");
+		System.out.println("hello");
 		
 	}
 	
 	
-=======
-
->>>>>>> branch 'master' of https://github.com/evaski/projet_sully_ia.git
-
->>>>>>> branch 'master' of https://github.com/evaski/projet_sully_ia.git
 }
 
 
