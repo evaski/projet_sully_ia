b0a26d7bc5a19049ddf326794bb37a2a95fbc567
evaski <evaski@live.fr>
2024-10-12 16:21:21 +0200

    dans le main pour tester les fonctions déjà faites

--- src/sully/SullyMain.java
+++ src/sully/SullyMain.java
@@ -1,6 +1,18 @@
 package sully ;
 
 public class SullyMain {
-	
+
+	 public static void main(String[] args) {
+		 Actions actions = new Actions(); 
+		 
+		 actions.avancer_de(30); 
+		 System.out.print("Test pour que le robot avance de 30cm");
+		 
+		 actions.ouvrir_pince(); 
+		 System.out.print("Test pour voir s'il ouvre les pinces");
+		 
+		 actions.fermer_pince(); 
+		 System.out.print("Test pour voir s'il ferme les pinces"); 
+	 }
 
 }
