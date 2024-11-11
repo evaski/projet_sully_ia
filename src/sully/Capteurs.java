1dc4e2e7eaa7a74ad0c44ed4bccbeb13295b5eb7
evaski <evaski@live.fr>
2024-11-11 16:53:31 +0100

    attention j'ai mis un getCouleur mais il faut le changej j'ai fait stocker distance et detecterDiscontinuité (pas testé)

--- src/sully/Capteurs.java
+++ src/sully/Capteurs.java
@@ -10,87 +10,106 @@ import lejos.robotics.SampleProvider;
 
 public class Capteurs {
 	// Constantes 
-		final static int nombreTotalPoints = 100;
-		final static int intervalleMS = 15;  
-		
-		// Attributs 
-			private double distanceInitiale;
-			private double distanceFinale;
-			private ArrayList<Double> distances;
-			private EV3UltrasonicSensor ultrasonicSensor;
-			private EV3TouchSensor pressionSensor;
-			
-			private Actions robot; 
+	final static int nombreTotalPoints = 100;
+	final static int intervalleMS = 15;  
 
-			// Constructeur
-			public Capteurs() {
-				// Initialisation des attributs
-				this.distanceInitiale = 0.0;
-				this.distanceFinale = 0.0;
-				ultrasonicSensor = new EV3UltrasonicSensor(SensorPort.S1);
-				pressionSensor = new EV3TouchSensor(SensorPort.S3);
-				distances = new ArrayList<>();
-			}
-			
+	// Attributs 
+	private double distanceInitiale;
+	private double distanceFinale;
+	private ArrayList<Double> distances;
+	private EV3UltrasonicSensor ultrasonicSensor;
+	private EV3TouchSensor pressionSensor;
 
-			public double getDistance() {
-				SampleProvider distanceProvider = ultrasonicSensor.getDistanceMode();
-				float[] sample = new float[distanceProvider.sampleSize()];
-				distanceProvider.fetchSample(sample, 0);
-				return sample[0]; // La distance mesurée en mètres
-			}
-			
-			// ATTENTION pour l'instant je peux stocker les distances que quand je tourne il faudra voir si on en a besoin a un autre moment 
-			public double tempsPourTourner(double angle) {
-				long startTime = System.currentTimeMillis(); // Démarre le chronomètre
-				robot.tourner_de(angle); 
-				long endTime = System.currentTimeMillis(); // Arrête le chronomètre
+	private Actions robot; 
 
-				long tempsEcoule = endTime - startTime; // Calcule le temps écoulé
-				return tempsEcoule; 
-			}
-			
-			public ArrayList<Double> stockerDistance(int temps){
-				
-				long startTime = System.currentTimeMillis();
-				distances.clear();
-		        while (System.currentTimeMillis() - startTime < temps) { 
-		        	distances.add(this.getDistance()); 
-		        	
-		        	try {
-		                Thread.sleep(intervalleMS);
-		            } catch (InterruptedException e) {
-		                e.printStackTrace();
-		            }
-				}
-		        // permet d'attendre le prochain intervalle 
-		        
-				return distances ; 
-			}
+	// Constructeur
+	public Capteurs() {
+		// Initialisation des attributs
+		this.distanceInitiale = 0.0;
+		this.distanceFinale = 0.0;
+		ultrasonicSensor = new EV3UltrasonicSensor(SensorPort.S2);
+		pressionSensor = new EV3TouchSensor(SensorPort.S3);
+		distances = new ArrayList<>();
+	}
 
-			public void closeUltrasonicSensor() {
-				ultrasonicSensor.close(); // Fermer le capteur lorsqu'il n'est plus nécessaire
-			}
 
-			public float get_pression() {
-				SampleProvider pressionProvider = pressionSensor.getMode(0); // Mode de lecture analogique
-				float[] sample = new float[pressionProvider.sampleSize()];
-				pressionProvider.fetchSample(sample, 0);
-				return sample[0];
-			}
-			
-			public void closepressionSensor() {
-				pressionSensor.close(); // Fermer le capteur lorsque ce n'est plus nécessaire
-			}
+	public double getDistance() {
+		SampleProvider distanceProvider = ultrasonicSensor.getDistanceMode();
+		float[] sample = new float[distanceProvider.sampleSize()];
+		distanceProvider.fetchSample(sample, 0);
+		return sample[0]; // La distance mesurée en mètres
+	}
 
-			// Méthode privée pour lire la distance (simule la lecture d'un capteur)
-			private double lireDistance() {
-				// Code pour lire la distance depuis le capteur à ultrasons
-				// Simule une valeur aléatoire pour l'exemple
-				return this.getDistance();  // À remplacer par la lecture réelle du capteur
+	public int getCouleur() {
+		return 0; 
+	}
+
+	// ATTENTION pour l'instant je peux stocker les distances que quand je tourne il faudra voir si on en a besoin a un autre moment 
+	public double tempsPourTourner(double angle) {
+		long startTime = System.currentTimeMillis(); // Démarre le chronomètre
+		robot.tourner_de(angle); 
+		long endTime = System.currentTimeMillis(); // Arrête le chronomètre
+
+		long tempsEcoule = endTime - startTime; // Calcule le temps écoulé
+		return tempsEcoule; 
+	}
+
+	public ArrayList<Double> stockerDistance(int temps){
+
+		long startTime = System.currentTimeMillis();
+		distances.clear();
+		while (System.currentTimeMillis() - startTime < temps) { 
+			distances.add(this.getDistance()); 
+
+			try {
+				Thread.sleep(intervalleMS);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
-			// Méthode pour calculer l'angle de rotation en degrés avec la trigonométrie
-			/*public double getAngleTrigo() {
+		}
+		// permet d'attendre le prochain intervalle 
+
+		return distances ; 
+	}
+
+	public List<Integer> detecterDiscontinuite(){
+		int l = distances.size();
+		List<Integer> indicesDiff = new ArrayList<>();
+		int seuil = 10; // A CHANGER JE SAIS PAS DU TOUT COMMENT ON VA DEFINIR CE SEUIL 
+
+		for (int i = 0; i< l; i++) {
+			float diff = (float) Math.abs(distances.get(i+1) - distances.get(i));
+			if (diff > seuil) {
+				indicesDiff.add(i);
+			}
+		}
+		return indicesDiff;
+	}
+
+
+	public void closeUltrasonicSensor() {
+		ultrasonicSensor.close(); // Fermer le capteur lorsqu'il n'est plus nécessaire
+	}
+
+	public float get_pression() {
+		SampleProvider pressionProvider = pressionSensor.getMode(0); // Mode de lecture analogique
+		float[] sample = new float[pressionProvider.sampleSize()];
+		pressionProvider.fetchSample(sample, 0);
+		return sample[0];
+	}
+
+	public void closepressionSensor() {
+		pressionSensor.close(); // Fermer le capteur lorsque ce n'est plus nécessaire
+	}
+
+	// Méthode privée pour lire la distance (simule la lecture d'un capteur)
+	private double lireDistance() {
+		// Code pour lire la distance depuis le capteur à ultrasons
+		// Simule une valeur aléatoire pour l'exemple
+		return this.getDistance();  // À remplacer par la lecture réelle du capteur
+	}
+	// Méthode pour calculer l'angle de rotation en degrés avec la trigonométrie
+	/*public double getAngleTrigo() {
 				// Mesure de la distance avant la rotation
 				this.distanceInitiale = this.lireDistance();
 				System.out.println("Distance initiale: " + distanceInitiale + " cm");
@@ -111,26 +130,26 @@ public class Capteurs {
 				System.out.printf("Angle de rotation: %.2f degrés%n", angleDegres);
 				return angleDegres;
 			}*/ 
-			
-			// Méthode pour calculer l'angle de rotation en degrés à partir du nombre de points mesurés
-			public double getAnglePoints(int nombrePointsMesures) {
-				// Vérification que le nombre total de points est supérieur à zéro
-				if (nombreTotalPoints <= 0) {
-					System.err.println("Le nombre total de points doit être supérieur à zéro.");
-					return -1; // Indiquer une erreur
-				}
-				// Vérification que le nombre de points mesurés est valide
-				if (nombrePointsMesures < 0 || nombrePointsMesures > nombreTotalPoints) {
-					System.err.println("Le nombre de points mesurés doit être compris entre 0 et " + nombreTotalPoints);
-					return -1; // Indiquer une erreur
-				}
-				// Calcul de l'angle par produit en croix
-				double angleMesure = ((double) nombrePointsMesures / nombreTotalPoints) * 360.0;
-				//System.out.printf("Angle mesuré: %.2f degrés%n", angleMesure);
-				return angleMesure;
-			}
-			
-			/* public double choisir_methode_mesure_dangle(int nombrePointsMesures) {
+
+	// Méthode pour calculer l'angle de rotation en degrés à partir du nombre de points mesurés
+	public double getAnglePoints(int nombrePointsMesures) {
+		// Vérification que le nombre total de points est supérieur à zéro
+		if (nombreTotalPoints <= 0) {
+			System.err.println("Le nombre total de points doit être supérieur à zéro.");
+			return -1; // Indiquer une erreur
+		}
+		// Vérification que le nombre de points mesurés est valide
+		if (nombrePointsMesures < 0 || nombrePointsMesures > nombreTotalPoints) {
+			System.err.println("Le nombre de points mesurés doit être compris entre 0 et " + nombreTotalPoints);
+			return -1; // Indiquer une erreur
+		}
+		// Calcul de l'angle par produit en croix
+		double angleMesure = ((double) nombrePointsMesures / nombreTotalPoints) * 360.0;
+		//System.out.printf("Angle mesuré: %.2f degrés%n", angleMesure);
+		return angleMesure;
+	}
+
+	/* public double choisir_methode_mesure_dangle(int nombrePointsMesures) {
 				// Calcul des angles à partir des deux méthodes
 				double angleTrigo = getAngleTrigo();
 				double anglePoints = getAnglePoints(nombrePointsMesures);
